528b687e81df4c4fd1e9ad3c0a610d8e1bc3fdb8
hasanrc24 <hasanrc24@gmail.com>
2023-03-16 16:21:09 +0600

    sd

--- script.js
+++ script.js
@@ -1,39 +1,54 @@
-let status = false;
-let min = 2000;
-let max = 6000;
+let min = 4000;
+let max = 8000;
 const button = document.getElementById("btn");
 const start = document.getElementById("start");
 const stops = document.getElementById("stop");
 
 let intervalId;
-let randomInterval;
+
+function randomNumber() {
+  let randomInterval = Math.floor(Math.random() * (max - min + 1)) + min;
+  return randomInterval;
+}
 
 function startReload() {
-  //   status = true;
-  //   start.style.display = "none";
-  //   stops.style.display = "block";
-  intervalId = setInterval(function () {
-    randomInterval = Math.floor(Math.random() * (max - min + 1)) + min;
-    console.log(randomInterval);
-    location.reload();
-  }, randomInterval);
+  start.style.display = "none";
+  stops.style.display = "block";
+  console.log(randomNumber());
+  if (!localStorage.getItem("isReloading")) {
+    localStorage.setItem("isReloading", true);
+    intervalId = setInterval(function () {
+      chrome.tabs.reload();
+    }, randomNumber());
+  } else {
+    intervalId = setInterval(function () {
+      chrome.tabs.reload();
+    }, randomNumber());
+  }
 }
 
 function stopReload() {
-  //   status = false;
-  //   stops.style.display = "none";
-  //   start.style.display = "block";
-  clearInterval(intervalId);
+  stops.style.display = "none";
+  start.style.display = "block";
+  if (localStorage.getItem("isReloading")) {
+    localStorage.removeItem("isReloading");
+    clearInterval(intervalId);
+  }
+}
+if (localStorage.getItem("isReloading")) {
+  startReload();
 }
 
 start.addEventListener("click", startReload);
 stops.addEventListener("click", stopReload);
-// function main() {
-//   if (status === true) {
-//     start.style.display = "none";
-//   } else {
-//     stops.style.display = "none";
-//   }
-// }
+function main() {
+  if (localStorage.getItem("isReloading")) {
+    start.style.display = "none";
+    stops.style.display = "block";
+  } else {
+    stops.style.display = "none";
+    start.style.display = "block";
+  }
+}
 
-// main();
+main();
